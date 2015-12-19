06c6f4c0b94a7b5b5e7dff0264c646cad80f17d4
KariWomack <kmw123@pitt.edu>
2015-12-19 03:46:17 -0500

    Push

---
+++ Website2015/webDevelopment/saidToggle.js
@@ -0,0 +1,37 @@
+var originalBackground;
+function toggleHighlight() {
+    var pos = this.getAttribute('input');
+    var color;
+    switch (pos) {
+        case 'nellNelson':
+        color = 'orange';
+        break;
+        case 'workingGirl':
+        color = 'pink';
+        break;
+        case 'female':
+        color = 'red';
+        break;
+        case 'male':
+        color = 'blue';
+        break; 
+    }
+    var status = this.checked;
+    // the status to which you've just changed the checkbox
+    var spans = document.getElementsByClassName(pos);
+    for (var i = 0; i < spans.length; i++) {
+        if (status == true) {
+            spans[i].style.backgroundColor = color;
+        } else {
+            spans[i].style.backgroundColor = originalBackground;
+        }
+    }
+}
+function init() {
+    originalBackground = document.body.style.backgroundColor;
+    var checkboxes = document.getElementsByTagName('value');
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].addEventListener('click', toggleHighlight, false);
+    }
+}
+window.addEventListener('DOMContentLoaded', init, false);
